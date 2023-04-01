94597df60cb4664cbc5c97048fd481547bf3896a
Sihan <swang943@gatech.edu>
2023-04-01 12:44:59 -0400

    link home, service, contact page

--- flaskr/app.py
+++ flaskr/app.py
@@ -41,6 +41,10 @@ def record_button():
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
   
 if __name__ == "__main__":
   app.run(debug = True)
