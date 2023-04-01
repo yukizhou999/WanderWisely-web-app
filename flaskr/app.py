e6bbcd45b29ea8c8b9da6c3392c0936c2fa2ff9f
Liangqu Chen <lchenbusiness@gmail.com>
2023-03-31 21:53:59 -0400

    app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+import pandas as pd
 
+# get Activities and Amenities
+activities = pd.read_csv("data/activity_related_parks.csv")
+activities = activities["name"].unique()
+
+amenities = pd.read_csv("data/amenity_related_parks.csv")
+amenities = amenities["name"].unique()
+
+print(activities)
+print("_---------------_")
+print(amenities)
 app = Flask(__name__)
+
+@app.route('/ActivitiesAndAmenities')
+def ActivitiesAndAmenities():
+    return render_template('ActivitiesAndAmenities.html')
 
 @app.route('/')
 def home():
