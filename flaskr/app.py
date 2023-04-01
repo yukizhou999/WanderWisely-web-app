66c7de7ae41902feaed0ccdced0440f90e48c9f7
Baiyan Ren <rbaiyan1969@gmail.com>
2023-04-01 14:41:09 -0500

    modify page 4

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from database import conn_to_db, sql_query
+import helper_functions as uf
 
 # build database connection
 
-conn, engine = conn_to_db()
-
+conn, engine = uf.conn_to_db()
 
 # get Activities and Amenities
-activities = sql_query("select * from wanderwisely.activity_related_parks", conn)
+activities = uf.import_data("select * from wanderwisely.activity_related_parks", conn)
 activities = activities["name"].unique()
-amenities = sql_query("select * from wanderwisely.amenity_related_parks", conn)
+amenities = uf.import_data("select * from wanderwisely.amenity_related_parks", conn)
 amenities = amenities["name"].unique()
 
 # record user's selection
-user_selection = {"activities":[], "amenities":[]}
+user_selection = {"activities": [], "amenities": [], "pois": []}
 
 
 def update_selection(selection, select_type):
@@ -22,29 +21,53 @@ def update_selection(selection, select_type):
         user_selection[select_type].remove(selection)
     else:
         user_selection[select_type].append(selection)
-    
+
 
 app = Flask(__name__)
 
+
 @app.route('/ActivitiesAndAmenities')
 def ActivitiesAndAmenities():
-    return render_template('ActivitiesAndAmenities.html',activities = activities, amenities = amenities)
+    return render_template('ActivitiesAndAmenities.html', activities=activities, amenities=amenities)
+
 
 @app.route('/record_button', methods=['POST'])
 def record_button():
     data = request.get_json()
     update_selection(data["input"], data["type"])
+    print(user_selection)
     # Record the button click in the database or perform any other action
     return '', 204
+
+
+def generate_places(parkCode, activities):
+    # todo modify sql query
+    parks_df = uf.import_data("select * from wanderwisely.activity_related_parks", conn)
+    parkName = parks_df[parks_df['parkCode'] == parkCode]['parkName'][0]
+    places_df = uf.import_data("select * from wanderwisely.things_to_do_places", conn)
+    filtered_places_df = places_df[
+        (places_df['parkCode'] == parkCode) & (places_df['activity_name'].isin(activities))].copy()
+    filtered_places = filtered_places_df['thing_title'].to_list()
+    return parkName, filtered_places
+
+
+@app.route('/poi')
+def poi():
+    activities = user_selection['activities']
+    # parkName, places = generate_places('acad', activities)
+    parkName, places = generate_places('acad', ['Biking', 'Hiking'])
+    return render_template('poi.html', parkName=parkName, places=places)
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-  
+
+
 if __name__ == "__main__":
-  app.run(debug = True)
+    app.run(debug=True)
--- flaskr/helper_functions.py
+++ flaskr/helper_functions.py
@@ -1,6 +1,7 @@
 import yaml
-
-
+import pymysql
+import pandas as pd
+from sqlalchemy import URL,create_engine
 
 
 class ConfigClass(object):
@@ -22,3 +23,26 @@ def load_yaml(yaml_file):
             return config
     except FileNotFoundError:
         print(f'ERROR - cannot find config file {yaml_file}')
+
+
+def conn_to_db():
+    ENDPOINT = "wanderwisely.chwnr0jplwmz.us-east-1.rds.amazonaws.com"
+    PORT = 3306
+    USER = "admin"
+    token = "6242WW2023"
+    conn = pymysql.connect(host=ENDPOINT, user=USER, passwd=token, port=PORT)
+
+    url_object = URL.create(
+        "mysql+pymysql",
+        username=USER,
+        password=token,  # plain (unescaped) text
+        host=ENDPOINT,
+        database="wanderwisely"
+    )
+
+    engine = create_engine(url_object)
+    return conn, engine
+
+
+def import_data(query, conn):
+    return pd.read_sql(query, con=conn)
